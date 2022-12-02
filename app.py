b70087f06377b9f18fb0d200585599e29b242981
Ahmed Ibrahem <ahmedibrahem00091@gmaill.com>
2022-12-02 18:04:47 +0200

    Added JWT protections on deleting items

--- app.py
+++ app.py
@@ -39,6 +39,19 @@ def create_app(db_url=None):
     app.config["JWT_SECRET_KEY"] = "secret_key"
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
     with app.app_context():
         db.create_all()
 
