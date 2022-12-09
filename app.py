6cd818b017d982866765aff9cc88bbc327e80c50
Ahmed Ibrahem <ahmedibrahem00091@gmaill.com>
2022-12-09 05:31:58 +0200

    Made the migrations on every build

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def create_app(db_url=None):
     app.config["API_VERSION"] = "v1"
     app.config["OPENAPI_VERSION"] = "3.0.3"
     app.config["OPENAPI_URL_PREFIX"] = "/"
-    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/"
     app.config[
         "OPENAPI_SWAGGER_UI_URL"
     ] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
